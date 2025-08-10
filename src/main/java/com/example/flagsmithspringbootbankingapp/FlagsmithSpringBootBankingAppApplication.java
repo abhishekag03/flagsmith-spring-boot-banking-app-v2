8f36188034f438f1a8ef586bb764cd9f81078b80
Abhishek Agarwal <abhishekag03@gmail.com>
2025-08-11 00:37:29 +0530

    repository init

---
+++ src/main/java/com/example/flagsmithspringbootbankingapp/FlagsmithSpringBootBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flagsmithspringbootbankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagsmithSpringBootBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlagsmithSpringBootBankingAppApplication.class, args);
+    }
+
+}
